7aefc980a2a2dbe4a8942657b031e5e842183bd5
dsimplichev <dsimplichev1948@gmail.com>
2023-02-08 13:59:14 +0000

    Data Types and Variables
    Free time to do some exercises.

---
+++ Data Types and Variables/01_echoType.js
@@ -0,0 +1,18 @@
+function echoType(param) {
+
+    const dataType = typeof (param);
+    console.log(dataType);
+
+    if (dataType == 'string' || dataType == 'number') {
+        console.log(param)
+    } else {
+        console.log('Parameter is not suitable for printing')
+    }
+
+
+
+
+
+
+}
+echoType(null)
---
+++ Data Types and Variables/03_rightPlace.js
@@ -0,0 +1,17 @@
+function rightPlace(string, replace, result){
+
+
+let replaceWord = string.replace('_',replace);
+
+if(replaceWord !== result){
+    console.log("Not Matched")
+} else {
+    console.log("Matched")
+}
+
+
+
+
+}
+rightPlace('Str_ng', 'I', 'Strong')
+rightPlace('Str_ng', 'i', 'String')
