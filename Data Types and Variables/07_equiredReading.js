ab8282331e2b1a566aa3e5850bb9e977ae10eb8b
dsimplichev <dsimplichev1948@gmail.com>
2023-02-17 23:50:01 +0000

    Create 07_equiredReading.js

---
+++ Data Types and Variables/07_equiredReading.js
@@ -0,0 +1,10 @@
+function equiredReading(pagesNumber, pagesPerHour,daysReading ){
+
+
+let totalTime = pagesNumber / pagesPerHour 
+
+ let hoursPerDay = totalTime / daysReading
+ 
+ console.log(hoursPerDay)
+}
+equiredReading(212,20,2)
