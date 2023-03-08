2a93e0b7f0b01bd3eb696ec044d5e104867d60f3
dsimplichev <dsimplichev1948@gmail.com>
2023-03-08 18:30:50 +0000

    Text Processing
     new tasks text processing

---
+++ Text Processing/01_printCharacters.js
@@ -0,0 +1,8 @@
+function printCharacters(word){
+
+for(const letter of word){
+    console.log(letter)
+}
+
+}
+printCharacters('AWord')
---
+++ Text Processing/02_substring.js
@@ -0,0 +1,9 @@
+function substring(word, start, count){
+
+
+let newWord = word.substring(start,count + start);
+console.log(newWord)
+
+
+}
+substring('ASentence', 1, 8)
