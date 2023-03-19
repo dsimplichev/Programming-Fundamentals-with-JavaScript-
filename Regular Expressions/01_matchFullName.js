b6c9f4c5e8917950342359ff64479451270ad28c
dsimplichev <dsimplichev1948@gmail.com>
2023-03-19 16:00:51 +0000

    Create 01_matchFullName.js

---
+++ Regular Expressions/01_matchFullName.js
@@ -0,0 +1,13 @@
+function matchFullName(text){
+
+    let pattern = /\b[A-Z][a-z]+ [A-Z][a-z]+\b/g
+    let result = text.match(pattern);
+
+    console.log(result.join(' '))
+
+
+
+
+
+}
+matchFullName('"Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan	Ivanov"')
